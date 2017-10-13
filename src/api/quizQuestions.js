1309a4bb845a507de500fa4d87c114b2d95f56ae
Cariny <perezcariny@gmail.com>
2017-10-12 17:29:59 -0700

    Initial Commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,138 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+
+
+var quizQuestions = [
+  {
+      question: "What is your spirit animal ? ",
+      answers: [
+          {
+              type: "Stark",
+              content: "Wolf"
+              
+          },
+  
+ 
+ 
+          {
+              type: "Lannister",
+              content: "Lion"
+          },
+          {
+              type: "Targaryen",
+              content: "Dragon"
+          }
+      ]
+  },
+  {
+      question: "What do you value most ? ",
+      answers: [
+          {
+              type: "Stark",
+              content: "Loyalty"
+          },
+          {
+              type: "Lannister",
+              content: "Money"
+          },
+          {
+              type: "Targaryen",
+              content: "Power"
+          }
+      ]
+  },
+  {
+      question: "Who's your favorite family member ?",
+      answers: [
+          {
+              type: "Stark",
+              content: "My Parent"
+          },
+          {
+              type: "Lannister",
+              content: "My Sibling"
+          },
+          {
+              type: "Targaryen",
+              content: "My Grandparent"
+          }
+      ]
+  },
+  {
+      question: "Choose an element",
+      answers: [
+          {
+              type: "Stark",
+              content: "Water"
+          },
+          {
+              type: "Lannister",
+              content: "Air"
+          },
+          {
+              type: "Targaryen",
+              content: "Fire"
+          }
+      ]
+  },
+  {
+      question: " Which Castle would you live in ? ",
+      answers: [
+          {
+              type: "Stark",
+              content: "Winterfell"
+          },
+          {
+              type: "Lannister",
+              content: "Casterly Rock"
+          },
+          {
+              type: "Targaryen",
+              content: "DragonStone"
+          }
+      ]
+  }
+];
+ 
+export default quizQuestions;

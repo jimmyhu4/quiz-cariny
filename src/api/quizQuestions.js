db316f67d8a1e60cff54993f37fa8e592f9b7fed
jimmyhu4 <chenhui.hu@gmail.com>
2020-03-01 11:29:46 -0500

    added random alternative answers

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -2,6 +2,12 @@ var minimum = 1;
 var maximum = 100;
 var int1 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 var int2 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+// swap integers to make sure int1 <= int2
+if (int1 < int2){
+    var tmp = int1
+    int1 = int2
+    int2 = tmp
+}
 var operators = [' + ', ' - ', ' x ', ' / '];
 var opidx = Math.floor(Math.random() * 4)
 
@@ -14,6 +20,13 @@ if (opidx == 0){
 } else if (opidx == 3){
     var true_answer = int1 / int2;
 }
+
+var min_possible_answer = Math.max(1, true_answer - 20)
+var max_possible_answer = true_answer + 20
+var possible_answer1 = Math.floor(Math.random()* (max_possible_answer-min_possible_answer+1)) + min_possible_answer
+var possible_answer2 = Math.floor(Math.random()* (max_possible_answer-min_possible_answer+1)) + min_possible_answer
+
+
 
 var quizQuestions = [
   {
@@ -45,11 +58,11 @@ var quizQuestions = [
           },
           {
               type: "Lannister",
-              content: "3"
+              content: possible_answer1
           },
           {
               type: "Targaryen",
-              content: "5"
+              content: possible_answer1
           }
       ]
   },

a653c3f3e7760cddb647183545ce82d984a2946d
jimmyhu4 <chenhui.hu@gmail.com>
2020-03-01 22:37:23 -0500

    updated Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -14,7 +14,7 @@ function Result(props) {
 			transitionAppearTimeout={500}
 		>
 			<div>
-				<strong>You {props.quizResult}</strong>!
+				<strong>You {props.quizResult}</strong>
 			</div>
 		</ReactCSSTransitionGroup>
 	);
